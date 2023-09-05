9d6b0225b23ba0b78d07b42e9510ae6a430277dd
mikeycb96 <michaelsonbaptist@gmail.com>
2023-09-05 19:18:14 +0100

    mvp amended

--- src/components/Country.js
+++ src/components/Country.js
@@ -1,56 +1,31 @@
-// import React, { useState } from 'react';
-
-// const Country = ({ country, onCountryVisited }) => {
-//   const [visited, setVisited] = useState(false); 
-
- 
-//   const toggleVisited = () => {
-//     setVisited(!visited);
-//     onCountryVisited(country, !visited);
-//   };
-
-//   return (
-//     <>
-//       <h2>{country.name.common}</h2>
-//       {country.flag}
-//       <p>Capital City: {country.capital}</p>
-      
-//       <label>
-//         Visited:
-//         <input
-//           type="checkbox"
-//           checked={visited} 
-//           onChange={toggleVisited} 
-//         />
-//       </label>
-//       <hr/>
-//     </>
-//   );
-// };
-
-// export default Country;
 import React, { useState } from 'react';
 
-const Country = ({ country, onCountryCheck }) => {
-    const [checked, setChecked] = useState(false);
+const Country = ({ country, onCountryVisited }) => {
+  const [visited, setVisited] = useState(false); 
 
-    const handleCheckboxChange = () => {
-        setChecked(!checked);
-        onCountryCheck(country, !checked); // Pass the checked state to the callback
-    };
+ 
+  const toggleVisited = () => {
+    setVisited(!visited);
+    onCountryVisited(country, !visited);
+  };
 
-    return (
-        <div>
-            <label>
-                <input type="checkbox" checked={checked} onChange={handleCheckboxChange} />
-                {country.name.common}
-                {country.flag}
-
-            </label>
-            <p>{country.capital}</p>
-            <hr />
-        </div>
-    );
+  return (
+    <>
+      <h2>{country.name.common}</h2>
+      {country.flag}
+      <p>Capital City: {country.capital}</p>
+      
+      <label>
+        Visited:
+        <input
+          type="checkbox"
+          checked={visited} 
+          onChange={toggleVisited} 
+        />
+      </label>
+      <hr/>
+    </>
+  );
 };
 
 export default Country;
