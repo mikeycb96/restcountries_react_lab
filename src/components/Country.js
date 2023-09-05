501befac9617b9cb4ce40c26157f31cab19c25fb
mikeycb96 <michaelsonbaptist@gmail.com>
2023-09-05 14:35:11 +0100

    created country.js

---
+++ src/components/Country.js
@@ -0,0 +1,15 @@
+const Country = ({country}) => {
+
+    return(
+        <>
+            <h2>{country.name.common}</h2>
+            <p>Capital City: {country.capital}</p>
+            <ul>
+               
+            </ul>
+        </>
+    )
+
+}
+
+export default Country;
